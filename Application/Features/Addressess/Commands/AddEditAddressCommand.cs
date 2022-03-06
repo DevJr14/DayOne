eea83f40a8bd8f92f97028135b2be4aa2ef60fb6
Junior Matlou <Junianohmm6@gmail.com>
2022-03-06 12:23:44 +0200

    Implemented add or edit addres request/command

--- Application/Features/Addressess/Commands/AddEditAddressCommand.cs
+++ Application/Features/Addressess/Commands/AddEditAddressCommand.cs
@@ -1,7 +1,7 @@
-﻿using MediatR;
+﻿using Application.Interfaces;
+using MediatR;
 using Shared.Requests.Address;
 using Shared.Wrappers;
-using System;
 using System.Threading;
 using System.Threading.Tasks;
 
@@ -14,10 +14,43 @@ namespace Application.Features.Addressess.Commands
 
     public class AddEditAddressCommandHandler : IRequestHandler<AddEditAddressCommand, Result<int>>
     {
+        private readonly IAddressRepo _repository;
+
+        public AddEditAddressCommandHandler(IAddressRepo repository)
+        {
+            _repository = repository;
+        }
+
         public async Task<Result<int>> Handle(AddEditAddressCommand request, CancellationToken cancellationToken)
         {
-            Console.WriteLine("Validation Passed!");
-            return await Result<int>.SuccessAsync("Validation passed");
+            if (request.AddressRequest.Id > 0)
+            {
+                //Edit
+                var address = await _repository.GetByIdAsync(request.AddressRequest.Id);
+                if (address == null)
+                {
+                    var response = await _repository.UpdateAsync(request.AddressRequest);
+                    if (response <= 0)
+                    {
+                        return await Result<int>.FailAsync("Failed to update new address.");
+                    }
+                    return await Result<int>.SuccessAsync(response, "Address successfully updated.");                    
+                }
+                else
+                {
+                    return await Result<int>.FailAsync($"Address with Id {request.AddressRequest.Id} not found.");
+                }
+            }
+            else
+            {
+                //Add new
+                var response = await _repository.AddAsync(request.AddressRequest);
+                if (response <= 0)
+                {
+                    return await Result<int>.FailAsync("Failed to create new address.");
+                }
+                return await Result<int>.SuccessAsync(response, "Address successfully created.");
+            }
         }
     }
 }
